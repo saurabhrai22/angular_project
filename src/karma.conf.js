f0467b7ecb432cb49989f6d09d71cf47e44145da
saurabh <s.e.rai@accenture.com>
2018-10-16 01:23:21 +0530

    angular

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,7 +7,8 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+	  require('karma-phantomjs-launcher'),
+     // require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -15,17 +16,29 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+	files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
+	preprocessors: {
+      './src/test.ts': ['@angular/cli']
+    },
+	mime: {
+      'text/x-typescript': ['ts', 'tsx']
+    },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
+    },
+	angularCli: {
+      environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     singleRun: false
   });
 };
